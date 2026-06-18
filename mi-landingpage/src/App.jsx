d6113dbc6d4c4d6eac6171324ca242a357b69f8c
vixo <vicente.orellana1512@gmail.com>
2026-06-18 12:31:44 -0400

    Update App.jsx

--- mi-landingpage/src/App.jsx
+++ mi-landingpage/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import Footer from './components/Footer'
 import './App.css'
+
 export default function App() {
   return (
     <>
@@ -15,3 +16,4 @@ export default function App() {
     </>
   )
 }
+
